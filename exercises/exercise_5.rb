1bb1c6070021d9284e98797ff36a1b82d592eaa1
TimaJuma <tima.xpl@gmail.com>
2020-09-15 14:12:29 -0600

    exercise 7

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum("annual_revenue")
+puts "Total revenue of all stores #{@total_revenue}"
+
+@average_revenue = @total_revenue/Store.count
+puts "Average revenue : #{@average_revenue}"
+
+@good_revenue = Store.where("annual_revenue >= 1000000").count
+puts "Number of stores with revenue over 1MLN: #{@good_revenue}"
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,17 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Fred", last_name: "Craveiro", hourly_rate: 60)
+@store1.employees.create(first_name: "Quinn", last_name: "Caume", hourly_rate: 60)
+@store1.employees.create(first_name: "Fedor", last_name: "Zlatanovic", hourly_rate:55)
+@store2.employees.create(first_name: "LiJackson", last_name: "Simon", hourly_rate: 60)
+@store2.employees.create(first_name: "Kernel", last_name: "Justice", hourly_rate: 60)
+@store2.employees.create(first_name: "Justin", last_name: "Manoryk", hourly_rate: 60)
+
+@all_employees = Employee.all_employees
+@all_employees.each do |employee|
+  puts "This is #{employee.first_name}"
+end
+
+puts "Total number of employees: #{Employee.count}"
