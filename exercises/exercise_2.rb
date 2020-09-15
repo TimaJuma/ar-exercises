ababd5e77366ffb80326dd7a8b6325810df060b5
TimaJuma <tima.xpl@gmail.com>
2020-09-15 12:58:52 -0600

    exercise 2 - update

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,12 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# UPDATE TABLE
+
+@store1 = Store.find(id: 1)
+@store2 = Store.find(id: 2)
+puts @store1
+
+@store1.name = "ChinaTOwn"
+print 'UPDATED the name of city'
+puts @store1.name
